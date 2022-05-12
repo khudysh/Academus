4af753d72b7116da7b9264f73ec96183a22faf20
Eugene <ehd2001@mail.ru>
2022-05-12 10:56:35 +0300

    refactor alert dialog to other files at each screen

--- lib/screens/attendance/attendance.dart
+++ lib/screens/attendance/attendance.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:srm_test/models/attendance/attendance.model.dart';
-import 'package:http/http.dart' as http;
-import 'package:srm_test/models/attendance/attendance_popup.model.dart';
-import 'dart:convert';
-import 'package:srm_test/resources/theme.dart';
 import 'package:srm_test/controllers/attendance/attendance_controller.dart';
 import 'package:srm_test/models/attendance/attendance_expand.model.dart';
+import 'package:srm_test/screens/attendance/showDialog.dart';
 
 Widget exppansionStudents(HttpService httpService, String id) {
   return FutureBuilder(
@@ -91,143 +88,7 @@ class Attendance extends StatelessWidget {
                                                     title: Text(
                                                         "${student.name} ${student.surname}"),
                                                                                 onTap: () {
-                              showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return FutureBuilder(
-                                      future: httpService.getAttendance(student.id),
-                                      builder: (BuildContext context,
-                                          AsyncSnapshot<List<AttendancePopupData>>
-                                              snapshot2) {
-                                        print(snapshot2);
-                                        if (snapshot2.hasData) {
-                                          List<AttendancePopupData> studentsPopup =
-                                              snapshot2.data!;
-                                          return AlertDialog(
-                                            insetPadding: EdgeInsets.symmetric(
-                                                horizontal: MediaQuery.of(context)
-                                                        .size
-                                                        .width /
-                                                    6,
-                                                vertical: 24),
-                                            contentPadding: EdgeInsets.zero,
-                                            content: Stack(
-                                              clipBehavior: Clip.none,
-                                              children: <Widget>[
-                                                Positioned(
-                                                  right: -15.0,
-                                                  top: -15.0,
-                                                  child: InkResponse(
-                                                    onTap: () {
-                                                      Navigator.of(context).pop();
-                                                    },
-                                                    child: const CircleAvatar(
-                                                      radius: 12,
-                                                      child: Icon(
-                                                        Icons.close_outlined,
-                                                        size: 18,
-                                                      ),
-                                                      backgroundColor:
-                                                          Colors.white,
-                                                    ),
-                                                  ),
-                                                ),
-                                                Column(
-                                                    mainAxisSize:
-                                                        MainAxisSize.min,
-                                                    children: <Widget>[
-                                                      Container(
-                                                        height: 60,
-                                                        width:
-                                                            MediaQuery.of(context)
-                                                                .size
-                                                                .width,
-                                                        decoration: BoxDecoration(
-                                                            color: CustomTheme
-                                                                .lightTheme
-                                                                .primaryColor
-                                                                .withOpacity(0.8),
-                                                            border: Border(
-                                                                bottom: BorderSide(
-                                                                    color: Colors
-                                                                        .grey
-                                                                        .withOpacity(
-                                                                            0.3)))),
-                                                        child: Center(
-                                                            child: Text(
-                                                                "${student.name} ${student.surname}",
-                                                                style: TextStyle(
-                                                                  color: CustomTheme
-                                                                      .lightTheme
-                                                                      .colorScheme
-                                                                      .onPrimary,
-                                                                  fontWeight:
-                                                                      FontWeight
-                                                                          .bold,
-                                                                  fontSize: 20,
-                                                                ))),
-                                                      ),
-                                                      Padding(
-                                                          padding: EdgeInsets.all(
-                                                              20.0),
-                                                          child: Container(
-                                                            decoration: BoxDecoration(
-                                                                border: Border.all(
-                                                                    color: Colors
-                                                                        .grey
-                                                                        .withOpacity(
-                                                                            0.2))),
-                                                            child:
-                                                                SingleChildScrollView(
-                                                              child: Column(
-                                                                children:
-                                                                    studentsPopup
-                                                                        .map(
-                                                                  (AttendancePopupData
-                                                                      studentPopup) {
-                                                                    return Column(
-                                                                        children: [
-                                                                          Card(
-                                                                            child: ListTile(
-                                                                                title: Text('Имя родителя:'),
-                                                                                subtitle: Text(
-                                                                                  studentPopup.attendance,
-                                                                                  style: TextStyle(fontSize: 18),
-                                                                                )),
-                                                                          ),
-                                                                          Card(
-                                                                            child: ListTile(
-                                                                                title: Text('Телефон родителя:'),
-                                                                                subtitle: Text(
-                                                                                  studentPopup.mark,
-                                                                                  style: TextStyle(fontSize: 18),
-                                                                                )),
-                                                                          ),
-                                                                          Card(
-                                                                            child: ListTile(
-                                                                                title: Text('Дополнительная информация:'),
-                                                                                subtitle: Text(
-                                                                                  studentPopup.lesson_date,
-                                                                                  style: TextStyle(fontSize: 18),
-                                                                                )),
-                                                                          ),
-                                                                        ]);
-                                                                  },
-                                                                ).toList(),
-                                                              ),
-                                                            ),
-                                                          )),
-                                                    ]),
-                                              ],
-                                            ),
-                                          );
-                                        } else {
-                                          return Center(
-                                              child: CircularProgressIndicator());
-                                        }
-                                      },
-                                    );
-                                  });
+                               showDialog( builder: (BuildContext context) { return ShowDialog(httpService: httpService,student: student,);  }, context: context);
                             },
                                                   ),
                                                 ],
--- lib/screens/students/students.dart
+++ lib/screens/students/students.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:srm_test/models/students/student.model.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:srm_test/resources/theme.dart';
+import 'package:srm_test/screens/students/showDialog.dart';
 import 'package:srm_test/controllers/students/students_controller.dart';
-import 'package:srm_test/models/students/student_popup.model.dart';
+
 
 class Students extends StatelessWidget {
   final HttpService httpService = HttpService();
@@ -35,132 +33,7 @@ class Students extends StatelessWidget {
                             showDialog(
                                 context: context,
                                 builder: (BuildContext context) {
-                                  return FutureBuilder(
-                                    future: httpService.getPopup(student.id),
-                                    builder: (BuildContext context,
-                                        AsyncSnapshot<List<StudentPopupData>>
-                                            snapshot2) {
-                                      print(snapshot2);
-                                      if (snapshot2.hasData) {
-                                        List<StudentPopupData> studentsPopup =
-                                            snapshot2.data!;
-                                        return AlertDialog(
-                                          insetPadding: EdgeInsets.symmetric(
-                                              horizontal: MediaQuery.of(context)
-                                                      .size
-                                                      .width /
-                                                  6,
-                                              vertical: 24),
-                                          contentPadding: EdgeInsets.zero,
-                                          content: Stack(
-                                            clipBehavior: Clip.none,
-                                            children: <Widget>[
-                                              Positioned(
-                                                right: -15.0,
-                                                top: -15.0,
-                                                child: InkResponse(
-                                                  onTap: () {
-                                                    Navigator.of(context).pop();
-                                                  },
-                                                  child: const CircleAvatar(
-                                                    radius: 12,
-                                                    child: Icon(
-                                                      Icons.close_outlined,
-                                                      size: 18,
-                                                    ),
-                                                    backgroundColor:
-                                                        Colors.white,
-                                                  ),
-                                                ),
-                                              ),
-                                              Column(
-                                                  mainAxisSize:
-                                                      MainAxisSize.min,
-                                                  children: <Widget>[
-                                                    Container(
-                                                      height: 60,
-                                                      width:
-                                                          MediaQuery.of(context)
-                                                              .size
-                                                              .width,
-                                                      decoration: BoxDecoration(
-                                                          color: CustomTheme
-                                                              .lightTheme
-                                                              .primaryColor
-                                                              .withOpacity(0.8),
-                                                          border: Border(
-                                                              bottom: BorderSide(
-                                                                  color: Colors
-                                                                      .grey
-                                                                      .withOpacity(
-                                                                          0.3)))),
-                                                      child: Center(
-                                                          child: Text(
-                                                              "${student.name} ${student.surname}",
-                                                              style: TextStyle(
-                                                                color: CustomTheme
-                                                                    .lightTheme
-                                                                    .colorScheme
-                                                                    .onPrimary,
-                                                                fontWeight:
-                                                                    FontWeight
-                                                                        .bold,
-                                                                fontSize: 20,
-                                                              ))),
-                                                    ),
-                                                    Padding(
-                                                        padding: EdgeInsets.all(
-                                                            20.0),
-                                                        child: Container(
-                                                          decoration: BoxDecoration(
-                                                              border: Border.all(
-                                                                  color: Colors
-                                                                      .grey
-                                                                      .withOpacity(
-                                                                          0.2))),
-                                                          child:
-                                                              SingleChildScrollView(
-                                                            child: Column(
-                                                              children:
-                                                                  studentsPopup
-                                                                      .map(
-                                                                (StudentPopupData
-                                                                    studentPopup) {
-                                                                  return Column(
-                                                                    children:[ Card(
-                                                                      child: ListTile(
-                                                                          title: Text(
-                                                                              'Имя родителя:'),
-                                                                              subtitle: Text(studentPopup.parent_name, style: TextStyle(fontSize: 18),)),
-                                                                    ),
-                                                                            Card(
-                                                                              child: ListTile(
-                                                                                                                                                    title: Text(
-                                                                              'Телефон родителя:'),
-                                                                              subtitle: Text(studentPopup.parent_phone, style: TextStyle(fontSize: 18),)),
-                                                                            ),
-                                                                            Card(
-                                                                              child: ListTile(
-                                                                                                                                                    title: Text(
-                                                                              'Дополнительная информация:'),
-                                                                              subtitle: Text(studentPopup.add_info, style: TextStyle(fontSize: 18),)),
-                                                                            ),
-                                                                    ]);
-                                                                },
-                                                              ).toList(),
-                                                            ),
-                                                          ),
-                                                        )),
-                                                  ]),
-                                            ],
-                                          ),
-                                        );
-                                      } else {
-                                        return Center(
-                                            child: CircularProgressIndicator());
-                                      }
-                                    },
-                                  );
+                                  return ShowDialog(httpService: httpService,student: student,);
                                 });
                           },
                         ),
